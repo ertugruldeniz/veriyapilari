b86d16b13755e6fdab83ad0d98640d26901ac973
ertugruldeniz <ertugruldeniz@outlook.com.tr>
2017-09-09 13:50:56 +0300

    Add files via upload
    Bağlı Listeye for döngüsü kullanarak basit düzeyde veri ekleme işlemleri.

---
+++ bagliliste2.cpp
@@ -0,0 +1,68 @@
+//DÖNGÜNELERÝ KULLANARAK BAÐLI LÝSTE YAPIMI
+#include <stdio.h>
+
+#include <stdlib.h>
+
+struct liste{
+	
+	
+	int data ;
+	
+	liste*next;
+	
+};
+
+
+typedef liste node;
+
+//link list içeriðini yazdýran fonksiyon
+void yazdir(node *iter){
+	int sayac=1;
+	while(iter!=NULL){
+		
+		printf("\n%d.incidegeri:%d ",sayac,iter->data);
+		iter=iter->next;
+		sayac++;
+	}
+}
+
+void ekle(node *iter,int x){
+	
+	while(iter->next!=NULL){
+	
+			iter=iter->next;
+		}
+		
+		//DÖNGÜ ÝLE BAÐLI LÝSTE SONUNA ELAMAN EKKETÝYORUZ 
+		iter->next=(node *)malloc(sizeof(node));
+		
+		iter->next->data=x;
+		
+		iter->next->next=NULL;
+			
+	
+}
+
+int main() {
+	
+	node *root ; //KÖK DÝZÝN
+	
+	
+	root=(node*)malloc(sizeof(node));
+	
+	root->data=5;
+	
+	root->next=NULL;
+	
+	int i=0;
+	
+	
+	for(i=0; i<5;i++){
+	
+		ekle(root,i*2);
+	}
+	
+
+	yazdir(root); // yazdir(root->next); de baþlatýlabilir
+	
+}
